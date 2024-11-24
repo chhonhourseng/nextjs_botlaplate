78c9a9937ceaf4268aae87808284cf51ce2058c6
Chhonhour Seng <chhonhourseng@gmail.com>
2024-11-24 14:32:31 +0700

    - init

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,21 +7,32 @@ import LanguageCubit from '@/data/providers/languageCubit';
 import { app } from '@/constant/general';
 import Column from '@/components/column';
 import { useRouter } from 'next/navigation';
+import LoginPage from '@/app/login/page';
 
 export default function HomePage() {
   useBloc(LanguageCubit);
 
   const router = useRouter();
-
+  if (app.isLogin) {
+    return (
+      <Container>
+        <Column className="gap-2.5">
+          <div className="headline2">{T.home.s}</div>
+          <Column className="gap-2.5">
+            <div className="body4">userId: {app.getUser!.id}</div>
+            <div className="body4">userName: {app.getUser!.username}</div>
+            <button className="button" onClick={() => app.logOut()}>{T.logOut.s}</button>
+          </Column>
+        </Column>
+      </Container>
+    );
+  }
   return (
     <Container>
-      <Column className='gap-2.5'>
-        <div className='headline2'>{T.home.s}</div>
-        {app.isLogin ? <Column className='gap-2.5'>
-          <div className='body4'>userId: {app.getUser!.id}</div>
-          <div className='body4'>userName: {app.getUser!.username}</div>
-          <button className='button' onClick={()=> app.logOut()}>{T.logOut.s}</button>
-        </Column> : <button className='button' onClick={()=> router.push('/login')}>{T.logIn.s}</button>}</Column>
+      <Column className="gap-2.5">
+        <div className="headline2">{T.home.s}</div>
+        <button className="button" onClick={() => router.push('/login')}>{T.logIn.s}</button>
+      </Column>
     </Container>
   );
 }
